3de673ecdf230f45b204ce3e81ae251b4bb2b08f
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-13 03:22:49 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,16 @@
 class MP3Importer 
   attr_accessor :path
+  attr_reader :files
   def initialize(file_path)
     @path = file_path
   end
   def files 
-    @files1 = Dir.entries(@path).select {|f| !File.directory? f}
-    @files1 = files.select{|file| file.to_s[-4..-1] == ".mp3"}
-    @files1 = mp3s.flatten
+    @files = Dir.entries(@path).select {|f| !File.directory? f}
+    @files = @files.select{|file1| file1.to_s[-4..-1] == ".mp3"}
+    @files = mp3s.flatten
   end
   def import 
-    @files1.each do |song|
+    @files.each do |song|
       Song.new_by_filename(song)
     end
   end
